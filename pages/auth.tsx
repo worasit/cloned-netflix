156812dd0d8a888d9502bd49314ee39cc92d33a5
worasit <worasit.daimongkol@thoughtworks.com>
2023-06-05 08:02:05 +0700

    Change to use Image component from NextJs to optimize the load time.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 const Auth = () => {
   const router = useRouter();
@@ -45,7 +46,13 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="Logo" className="h-12" />
+          <Image
+            src="/images/logo.png"
+            width={200}
+            height={100}
+            alt="Logo"
+            className="h-12"
+          />
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
